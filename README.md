81e3f2b5580ae0f1ca75581a273d62499fbd5a39
ʀᴇxɪɴᴀᴢᴏʀ <85327329+Rexinazor@users.noreply.github.com>
2021-11-23 17:00:40 +0530

    PHP

---
+++ README.md
@@ -0,0 +1,24 @@
+# Bin-Checker-Bot
+
+A simple php telegram bot to check if the bin is valid or not
+
+## HOW TO DEPLOY?
+
+**FORK THE REPO FROM** [HERE](https://github.com/Benchamxd/Bin-Checker/fork)
+
+``ADD YOU BOT TOKEN IN LINE 12``
+
+**THEN DEPLOY YOUR REPO BY** ``https://heroku.com/deploy?template=<ur git link>``
+
+**AFTER DEPLOY SET WEBHOOK BY** ``api.telegram.org/bot<ur bot token>/setwebhook?url=<ur heroku link>/bot.php``
+
+**AFTER THIS REMOVE YOUR BOT TOKEN FROM THE REPO FOR YOUR SAFTEY**
+
+## HELP ME
+
+For any type of help on deploy. Contact me here [INDUS CHATS](https://t.me/induschats).
+
+
+##
+
+**DO GIVE A STAR TO MY PROJECT AND SUPPORT IT**
---
+++ bot.php
@@ -0,0 +1,103 @@
+<?php
+/*
+BY:- @BenchamXD
+
+CHANNEL:- @IndusBots
+*/
+error_reporting(0);
+
+set_time_limit(0);
+
+flush();
+$API_KEY = 'Your bot token'; //Your token
+##------------------------------##
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+ function sendmessage($chat_id, $text, $model){
+	bot('sendMessage',[
+	'chat_id'=>$chat_id,
+	'text'=>$text,
+	'parse_mode'=>$mode
+	]);
+	}
+	
+//==============BENCHAM======================//
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$message_id = $update->message->id;
+$chat_id = $message->chat->id;
+$name = $from_id = $message->from->first_name;
+$from_id = $message->from->id;
+$text = $message->text;
+$fromid = $update->callback_query->from->id;
+$username = $update->message->from->username;
+$chatid = $update->callback_query->message->chat->id;
+if($text == '/start')
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"***Welcome to bin checker
+
+Use*** `/bin xxxxx` ***to check the bin.
+
+You can also make a bot like this from here:- https://github.com/BenchamXd/Bin-Checker***",
+'parse_mode'=>"MarkDown",
+]);
+if(strpos($text,"/bin") !== false){ 
+$bin = trim(str_replace("/bin","",$text)); 
+
+$data = json_decode(file_get_contents("https://lookup.binlist.net/$bin"),true);
+$bank = $data['bank']['name'];
+$country = $data['country']['alpha2'];
+$currency = $data['country']['currency'];
+$emoji = $data['country']['emoji'];
+$scheme = $data['scheme'];
+$Brand = $data['brand'];
+$type = $data['type'];
+ if($data['scheme']){
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"***VALID BIN✅
+                
+Bin: $bin
+
+Type: $scheme
+
+Brand : $Brand
+
+Bank: $bank
+
+Country: $country $emoji
+
+Currency: $currency
+
+Credit/Debit:$type
+
+Checked By @$username***",
+'parse_mode'=>"MarkDown",
+]);
+    }
+else {
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"INVALID BIN❌",
+               
+]);
+}
+}
+/*
+BY:- @BenchamXD
+
+CHANNEL:- @IndusBots
+*/
